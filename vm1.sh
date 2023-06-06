ed177385bf6ad3f9ae1dc6066cda8447c7a4aa3d
azcloudberg <120568586+azcloudberg@users.noreply.github.com>
2023-06-06 17:32:08 +0530

    Update vm1.sh

--- vm1.sh
+++ vm1.sh
@@ -4,8 +4,6 @@ sudo apt-get update
 sudo apt-get install python3
 sudo apt-get install python3-pip
 python3 -m pip install -U pip
-pip3 install azure-storage-blob
-pip3 install azure-storage-queue
-pip3 install azure-storage-file-share
+sudo pip3 install azure-storage-blob azure-storage-file-share azure-storage-file-datalake azure-storage-queue
 pip3 install configparser
-pip3 install flask
+sudo pip3 install flask
