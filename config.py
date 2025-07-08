b5fef0c1c82d068f636bdab04e905d81a5a0bb78
renu160490 <renu.singh70518@gmail.com>
2025-07-08 17:12:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Account name
 account =projectsg16
 # Azure Storage account access key
-key =10cthiqNfjVNv0UpJkGY3X/Q4ouBd9hWxCjfWpkq0/SrUTSCoXHOfgHOlCrzDhoqQZf3HqlAAagF+AStYblMWw==
+key =10cthiqNfjVNv0UpJkGY3X/Q4ouBd9hWxCjfWpkq0/SrUTSCoXHOfgHOlCrzDhoqQZf3
 # Container name
 container =upload
